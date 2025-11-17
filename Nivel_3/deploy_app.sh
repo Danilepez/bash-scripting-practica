a8be05fc2c8e307fa62254f794e0abb7d7283588
Daniel <danilepez@gmail.com>
2025-11-16 23:59:22 -0400

    changes

--- Nivel_3/deploy_app.sh
+++ Nivel_3/deploy_app.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-# Nombre del archivo: deploy_app.sh
-# Ubicación: Nivel_3/
-
 REPO_URL="https://github.com/rayner-villalba-coderoad-com/clash-of-clan"
-APP_DIR="/var/www/clash-of-clan" # Directorio donde se desplegará
+APP_DIR="/var/www/clash-of-clan" 
 LOG_FILE="deploy.log"
-SERVICE_NAME="nginx" # O el servicio que uses, como 'apache2' o 'pm2'
+SERVICE_NAME="nginx"
 TIMESTAMP=$(date +"%Y-%m-%d %H:%M:%S")
 
 echo "[$TIMESTAMP] --- INICIO del Despliegue ---" >> "$LOG_FILE"
@@ -55,7 +52,7 @@ fi
 echo "[$TIMESTAMP] --- FIN del Despliegue ---" >> "$LOG_FILE"
 echo "$DEPLOY_MESSAGE"
 
-# BONUS: Envío de notificación (ejemplo con webhook de Discord/Slack - requiere 'curl')
-# WEBHOOK_URL="TU_WEBHOOK_URL_AQUI"
-# NOTIF_PAYLOAD='{"content": "Despliegue de Clash of Clan: '$DEPLOY_MESSAGE'"}'
-# curl -H "Content-Type: application/json" -d "$NOTIF_PAYLOAD" "$WEBHOOK_URL"
+BONUS: Envío de notificación (ejemplo con webhook de Discord/Slack - requiere 'curl')
+WEBHOOK_URL="https://discord.com/api/webhooks/1439820804723445893/RoeSRhAEmpOKrrCXUsf66jPATTL-iiOYDyOcrAoutrjnPfeJDH6xFi2RVQUGnMgHdA6i"
+NOTIF_PAYLOAD='{"content": "Despliegue de Clash of Clans: '$DEPLOY_MESSAGE'"}'
+curl -H "Content-Type: application/json" -d "$NOTIF_PAYLOAD" "$WEBHOOK_URL"
